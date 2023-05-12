92397a9733862a94df9e55f463ea557091663e92
AMIT KUMAR <93047022+oiiakm@users.noreply.github.com>
2023-05-13 02:59:10 +0530

    Lab 4 completed

---
+++ Lab 4/Le4_p4/app/src/main/java/com/example/le4_p4/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.le4_p4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b=findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creating a Toast message
+                Toast.makeText(getApplicationContext(), "Hello Noob\nWelcome Back!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
